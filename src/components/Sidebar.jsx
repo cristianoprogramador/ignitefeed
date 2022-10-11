6f4c9f23fdcb4d08bfb705693c756ab33af509ac
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-11 17:11:37 -0300

    Finalizado o Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
-export function Sidebar(){
-  return(
-    <aside className={styles.sidebar}></aside>
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/102186472?v=4" alt="" />
+
+        <strong>Cristiano Pereira da Silva</strong>
+        <span>Fullstack DEV</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
   )
 }
