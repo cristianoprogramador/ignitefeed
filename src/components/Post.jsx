0624cec68e06b873febc7cc1e253d586372e2960
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-12 15:28:09 -0300

    Botão de Aplaudir habilitado para utilizar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,6 +14,8 @@ export function Post({ author, publishedAt, content }) {
 
   const [newCommentText, setNewCommentText] = useState('')
 
+  console.log(newCommentText)
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -31,7 +33,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório')
   }
 
   function deleteComment(commentToDelete) {
@@ -42,6 +49,7 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne)
   }
 
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -80,10 +88,12 @@ export function Post({ author, publishedAt, content }) {
           placeholder='Deixe o seu comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
