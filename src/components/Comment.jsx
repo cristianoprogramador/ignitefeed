12511c61683ee27b1782db48ecc03b94419d6f07
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-12 13:57:47 -0300

    Conceito de imutabilidade inserindo a funcionalidade de deletar um comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({content}) {
+export function Comment({content, onDeleteComment}) {
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/102186472?v=4" alt="minha-foto" />
@@ -16,7 +20,7 @@ export function Comment({content}) {
               <time title='11 de Outubro de 2022' dateTime='2022-10-11 08:13:38' >Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    // imutabilidade => as variaveis não sofrem mutação, nos criamos um novo valor (um novo espaço na memoria)
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+    setComments(commentsWithoutDeletedOne)
+  }
+
 
   return (
     <article className={styles.post}>
@@ -56,9 +64,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p> <a href="#">{line.content}</a></p>
+            return <p key={line.content}> <a href="#">{line.content}</a></p>
 
           }
         })}
@@ -81,7 +89,11 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
         })}
       </div>
     </article>
